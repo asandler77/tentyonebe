ae294002ab332d050aef0a5243e4cb120a1e82d0
asandler77 <shwartz.alexey77@gmail.com>
2025-07-28 21:51:33 +0300

    init be

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
- 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/ask", methods=["POST"])
+def ask_question():
+    data = request.get_json()
+    question = data.get("question", "").lower()
+    print(f"[API] Получен вопрос: {question}")
+    if "это животное?" in question:
+        return jsonify(answer="Верно")
+    else:
+        return jsonify(answer="Неверно")
+
+if __name__ == "__main__":
+    app.run(debug=True)
